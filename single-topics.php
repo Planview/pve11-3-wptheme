b6db7d2e5c2ac84b1ccf292bbdf47eee077df8ea
jwitten428 <jwitten@planview.com>
2015-01-14 13:44:32 -0600

    Update single-topics.php
    add the function to pull the content for the subtitle

--- single-topics.php
+++ single-topics.php
@@ -10,6 +10,7 @@ get_header(); ?>
                 <h1>
                     <?php the_title(); ?>
                 </h1>
+                    <?php the_content( ); ?>
             </div>
         </div>
         <div class="video">
